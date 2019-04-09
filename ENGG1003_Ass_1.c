1cb6b8677463489566ed5456e218d16d6ced3bfe
Jarrad  Turner <c3282818@uon.edu.au>
2019-04-09 02:46:20 +0000

    Lab progress week 7

---
+++ ENGG1003_Ass_1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+int f;
+int e;
+int d;
+//printf("Hello world\n");
+
+for(int i=65; i<91; i++)
+ {
+    f = i -65; 
+    
+    printf("%c = %d", i, f);
+    
+    e = (f - 1);                   // k is (-1)
+    
+    if (e < 0)
+    {
+        e = 25;
+    }
+    printf("       %c is now %d\n", i, e);
+    
+    d = (e + 1);
+    
+    if (d > 25)
+    {
+        d = 0;
+    }
+    printf("                          %c is again %d\n", i, d);
+ }
+
+
+
+return(0);
+}
