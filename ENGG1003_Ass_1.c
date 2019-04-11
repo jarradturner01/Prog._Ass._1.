3372a822643c97a693d1d64ec5449600f381e979
Jarrad  Turner <c3282818@uon.edu.au>
2019-04-11 04:39:58 +0000

    yeet

--- ENGG1003_Ass_1.c
+++ ENGG1003_Ass_1.c
@@ -3,24 +3,18 @@
 int main(){
 int f;
 int e;
-int d;
+int k;
 int i;
 int x;
 //printf("Hello world\n");
 
 printf("Please select one of the following:\n1- encrytion with rotation cipher (given text + key)\n");
 
-scanf("%d", &x);
+//scanf("%d", &x);
+//printf("%d", x);
 
-switch (x){
-    case 1:
-            printf("Please select a variable to use as a key- \n");\
-            scanf("%d", &k);
-            for(int i=65; i<91; i++){
-                f = i - 65;
-                printf("%c = %d", i, f)
-            }
-}
+
+
   
 
 
