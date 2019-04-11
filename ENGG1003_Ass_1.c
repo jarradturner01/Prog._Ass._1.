b498f9f131823400910be9772c4c5834a277c32d
Jarrad  Turner <c3282818@uon.edu.au>
2019-04-11 04:31:30 +0000

    yeet

--- ENGG1003_Ass_1.c
+++ ENGG1003_Ass_1.c
@@ -10,7 +10,7 @@ int x;
 
 printf("Please select one of the following:\n1- encrytion with rotation cipher (given text + key)\n");
 
-scanf("%d, &x);")
+scanf("%d", &x);
 
 switch (x){
     case 1:
@@ -23,35 +23,6 @@ switch (x){
 }
   
 
-
-for(int i=65; i<91; i++)
- {
-    f = i -65; 
-    
-   // printf("%c = %d", i, f);
-    
-    e = (f - 1);                   // k is (-1)
-    
-    if (e < 0)
-    {
-        e = 25;
-    }
-    //printf("       %c is now %d\n", i, e);
-    
-    d = (e + 1);
-    
-    if (d > 25)
-    {
-        d = 0;//
-    }
-  //  printf("                          %c is again %d\n", i, d);
- }
-
-for ( d = 0; d < 26; d++) 
-{
-  //
-  printf("%c = %d\n", i, d);
-}
 
 
 return(0);
